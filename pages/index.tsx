8005aba3800331316f7d81d7eaec2e39de00ca65
yeonsubak <yeonsubak@gmail.com>
2022-09-17 14:44:31 +0900

    Add & configured Flowbite

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <button className='m-4 p-4 rounded-full border border-blue-500'>Hello World</button>
+      <button className="m-4 rounded-full border border-blue-500 p-4">
+        Hello World
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
