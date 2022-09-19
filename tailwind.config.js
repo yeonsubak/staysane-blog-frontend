4b6b24d4c17570f9f67d83a613e2c716150d1f5c
yeonsubak <yeonsubak@gmail.com>
2022-09-19 16:30:21 +0900

    Complete navBar design / Add & apply global fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ['Noto Sans KR', 'sans-serif'],
+      serif: ['Noto Serif KR', 'serif'],
+    },
+    colors: {
+      'themeGray': '#F9F9FE',
+    },
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [
+    require("flowbite/plugin"),
+    require('flowbite-typography'),
+  ],
 };
