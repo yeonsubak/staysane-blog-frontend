d49dc3e911cc5573f2da2880f1e4ae4fdb6e4085
yeonsubak <yeonsubak@gmail.com>
2022-09-19 17:26:59 +0900

    Complete wireframing: BlogPost.tsx

---
+++ src/components/BlogPost.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const BlogPost = () => {
+  return (
+    <div className="mt-2 h-[326px] bg-white px-16 py-9 flex flex-col gap-6 shadow-lg">
+      <div className="flex flex-row">
+        <div className="imageBox h-24 w-24 shrink-0 bg-gray-600"></div>
+        <div className="ml-3">
+          <h2 className="h-20 break-words text-2xl font-semibold">
+            젠킨스로 CI/CD 파이프라인 구축하기 (3)
+          </h2>
+          <div className="postInfoBox flex flex-row gap-4 text-xs">
+            <span className="">2022년 9월 19일</span>
+            <span className="">2분 분량</span>
+            <span className="">2</span>
+            <span className="">365</span>
+          </div>
+        </div>
+      </div>
+      <div className="paragraphs text-sm">
+        <p className="line-clamp-3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nisi,
+          feugiat at sociis auctor lectus ultrices odio. At vel imperdiet congue
+          nibh est suspendisse. Pellentesque ut ornare non maecenas. Sapien
+          egestas sem viverra risus sed interdum. Velit rhoncus quam massa eu
+          cras pellentesque. Quam nunc, duis cursus vel.
+        </p>
+      </div>
+      <div className="postBoxFooter flex flex-row">
+        <div className="hashtagBox flex gap-6 font-semibold">
+          <button className="bg-themeGray px-3 py-2 shadow-md text-sm"># Jenkins</button>
+          <button className="bg-themeGray px-3 py-2 shadow-md text-sm"># CI/CD</button>
+          <button className="bg-themeGray px-3 py-2 shadow-md text-sm"># AWS</button>
+        </div>
+        <div className="avatarBox flex flex-row ml-auto gap-2 items-center">
+          <img src="/img/ProfileImage.jpg" alt="Profile image of the blog owner."  className="text-xs w-10 h-10 line-clamp-1 rounded-full"/>
+          <p className="font-semibold">Yeonsu Bak</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPost;
---
+++ src/pages/index.tsx
@@ -0,0 +1,52 @@
+import { Flowbite, Navbar } from "flowbite-react";
+import { NavbarToggle } from "flowbite-react/lib/esm/components/Navbar/NavbarToggle";
+import type { NextPage } from "next";
+import BlogPost from "../components/BlogPost";
+import StaysaneLogo from "../components/StaysaneLogo";
+
+const Home: NextPage = () => {
+  return (
+    <Flowbite
+      theme={{
+        theme: {
+          navbar: {
+            base: 'bg-themeGray',
+            collapse: {
+              list: 'mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-lg font-medium'
+            }
+          }
+        }
+      }}>
+      <div className="bg-themeGray">
+        <div className="mx-16 my-2 lg:mx-36">
+          <Navbar>
+            <Navbar.Brand>
+              <StaysaneLogo />
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+              <Navbar.Link href="#">
+                About
+              </Navbar.Link>
+              <Navbar.Link href="#">
+                Blog
+              </Navbar.Link>
+              <Navbar.Link href="#">
+                Contact
+              </Navbar.Link>
+            </Navbar.Collapse>
+          </Navbar>
+          <div className="grid grid-cols-5 gap-4 mt-2 h-screen">
+            <div className="col-span-3">
+              <BlogPost />
+            </div>
+            <div className="col-span-2">Div2</div>
+          </div>
+        </div>
+
+      </div>
+    </Flowbite>
+  );
+};
+
+export default Home;
