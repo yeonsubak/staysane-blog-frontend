f54d74fa96ab93d11a3a21b51eb4070d68791d11
yeonsubak <yeonsubak@gmail.com>
2022-09-29 03:08:09 +0900

    Modify layout of /about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,10 +6,10 @@ const About = ({}: any) => {
       <Head>
         <title>About | Stay Sane, Think Straight.</title>
       </Head>
-      <div className="mb-6 h-[82vh] bg-white px-6 py-8 shadow-lg sm:min-w-[680px] sm:px-10">
+      <div className="h-[83vh] shadow-md">
         <iframe
           src="https://v1.embednotion.com/embed/2a16f89746544d17a08649a066a98a9f"
-          style={{ width: "100%", height: "75vh", padding: "none" }}
+          style={{ width: "100%", height: "85vh" , padding: "none", display: "block" }}
         ></iframe>
       </div>
     </div>
