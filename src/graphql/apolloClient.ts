406da142f59e46be6a908eec4c13ce977ae4972b
yeonsubak <yeonsubak@gmail.com>
2022-10-18 03:50:12 +0900

    Fix target GraphQL URI to deployed strapi server

--- src/graphql/apolloClient.ts
+++ src/graphql/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: "http://localhost:8564/graphql",
+  uri: "https://strapi.staysane.me/graphql",
   cache: new InMemoryCache(),
 })
 
