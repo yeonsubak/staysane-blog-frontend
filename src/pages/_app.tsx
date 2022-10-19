d9ce64cde3bf4275d15cea3922318300f4738d90
yeonsubak <yeonsubak@gmail.com>
2022-10-19 21:18:58 +0900

    Add and configure 'nextjs-google-analytics' library to substitute legacy GA scripts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,33 +2,17 @@ import "/styles/globals.css";
 import "/styles/edjs.css";
 import type { AppProps } from "next/app";
 import TopNavBarDesktop from "../components/navBar/TopNavBarDesktop";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Twirl as Hamburger } from "hamburger-react";
 import TopNavBarMobile from "../components/navBar/TopNavBarMobile";
-import { useRouter } from "next/router";
-import * as gtag from "../lib/gtag";
-import Script from "next/script";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [isOpen, setOpen] = useState<boolean>(false);
-  
-  // Required functions for Google Analytics
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url: any) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    router.events.on("hashChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-      router.events.off("hashChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <div>
+      <div id="TopNavBarMobile">
         {isOpen ? <TopNavBarMobile isOpen={isOpen} setOpen={setOpen} /> : ""}
       </div>
       <div className="w-auto px-4 py-4 sm:px-8 md:mx-auto lg:w-[1024px]">
@@ -38,26 +22,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             <Hamburger toggled={isOpen} toggle={setOpen} />
           </div>
         </div>
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-        />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
+        <GoogleAnalytics trackPageViews />
         <Component {...pageProps} />
       </div>
     </div>
